9faa81e39f3d815e3de059c6aa4dccbf2944b0f2
ZzqiZQute <ziqiu-z@163.com>
2022-09-10 17:52:36 +0800

    fix

--- scripts/build-tc.js
+++ scripts/build-tc.js
@@ -1,6 +1,42 @@
 import execa from 'execa'
 import fs from 'fs-extra'
 import archiver from 'archiver'
+import path from 'path'
+
+function getAllDeps(p, paths) {
+    let name
+    let q = p
+    if (q !== '.') {
+        while (true) {
+            name = path.join(q, 'package.json')
+            if (fs.existsSync(name)) {
+                break
+            }
+            const parts = q.split('node_modules/')
+            parts.splice(parts.length - 2, 1)
+            q = parts.join('node_modules/')
+            if (!q) {
+                break
+            }
+        }
+    } else {
+        name = path.join(q, 'package.json')
+    }
+    if (paths.indexOf(q) > -1) {
+        return
+    }
+    if (q !== '.') {
+        paths.push(q)
+    }
+    const pkg = JSON.parse(fs.readFileSync(name).toString())
+    const deps = pkg.dependencies
+    if (!deps) {
+        return
+    }
+    Object.keys(deps).forEach((it) => {
+        getAllDeps(path.join(p, 'node_modules', it), paths)
+    })
+}
 
 ;(async () => {
     console.log('正在构建腾讯云函数资源中，请耐心等待~')
@@ -13,7 +49,12 @@ import archiver from 'archiver'
     await fs.copy('package.json', 'build-tc/package.json')
     await fs.writeFile('build-tc/package.json', (await fs.readFile('build-tc/package.json')).toString().replace(/"type": "module"/, '"type": "commonjs"'))
     await execa('npm', ['i', '--no-save'])
-    await fs.copy('node_modules', 'build-tc/node_modules')
+    const deps = []
+    getAllDeps('.', deps)
+    for (const dep of deps) {
+        await fs.copy(dep, `build-tc/${ dep }`)
+        await execa('babel', [dep, '-d', `build-tc/${ dep }`])
+    }
     await fs.writeFile('build-tc/index.js', `
 const execa = require('execa')
 
